394271a55f451565fb38cb0f76aa1dc77b1ade0f
cheetah5900 <cheetah5900@windowslive.com>
2025-12-27 17:33:47 +0700

    fix port

--- run_waitress.py
+++ run_waitress.py
@@ -3,5 +3,5 @@ from waitress import serve
 from betmanager.wsgi import application
 
 if __name__ == "__main__":
-    print("Starting Waitress server on http://0.0.0.0:8000")
-    serve(application, host='0.0.0.0', port=8000, threads=10)
+    print("Starting Waitress server on http://0.0.0.0:8001")
+    serve(application, host='0.0.0.0', port=8001, threads=10)
